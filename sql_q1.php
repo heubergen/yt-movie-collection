cda525ab1e2175cb84763ffa80216d408775ed4e
heubergen <github@z33.ch>
2016-01-24 00:46:14 +0100

    Fixing error and starting to show data from database.

--- sql_q1.php
+++ sql_q1.php
@@ -1,17 +1,24 @@
 <?php
-   //connect to database and select jm3_test
-   $con = new mysqli("localhost", "jm3", "ma7duiy7shioL","")or die('Could not connect: ' . mysql_error());
-   mysqli_select_db($con, "jm3_test");
-echo "Successfully connect to database";
-echo "<br>";
-$type = "test";
-                 //Insert JSON to MySQL Database
-                 $sql = "INSERT INTO tbl_2 (test, test2)
-                         VALUES('$type', '$type')";
-                         if(!mysqli_query($con, $sql))
-                {
-                        die('Error : ' . mysqli_error($con));
-		}
+   //connect to database and select tbl_ext
+   $con = new mysqli("HOST_HERE", "USER_HERE", "PASSWORD_HERE","")or die('Could not connect: ' . mysql_error());
+   mysqli_select_db($con, "DB_HERE");
+
+                 //Read data from database
+$sql = "SELECT type, title, genre FROM DB_HERE";
+$result = mysqli_query($con, $sql);
+
+// Show mysql error
+if (!$result) {
+    die(mysqli_error($con));
+}
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "type: " . $row["type"]. " - Title: " . $row["title"]. " - Genre: " . $row["genre"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
 mysqli_close($con);
-echo "Successfully write data in database";
 ?>
