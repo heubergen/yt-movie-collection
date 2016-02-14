03e7cce2de5c9b9d06b17dd6c3fdd5619346653d
heubergen <github@z33.ch>
2016-02-14 23:16:35 +0100

    Add structur note and clean up api-ext_info

--- api-ext_info.php
+++ api-ext_info.php
@@ -1,7 +1,6 @@
 <?php
 //load sql_con.php file
 include 'sql_con.php';
-//select data from table tbl_set and save it in $row
 $stmt = $conn->query("SELECT UNIX_TIMESTAMP(rating_date) FROM tbl_movie");
 $minusdate = strtotime('-10 days midnight');
 foreach ($stmt as $key) {
